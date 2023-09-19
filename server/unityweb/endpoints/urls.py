39fc91f80a2436e0f0634673c44807a71df51725
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-20 00:47:33 +0530

    add users api

--- server/unityweb/endpoints/urls.py
+++ server/unityweb/endpoints/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import posts
+from .views import posts, users
 
 urlpatterns = [
     path("posts/", posts, name="posts"),
+    path("users/", users, name="users"),
 ]
--- server/unityweb/endpoints/views.py
+++ server/unityweb/endpoints/views.py
@@ -1,13 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import PostSerializer
+from django.contrib.auth.models import User
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer, UserSerializer
 # Create your views here.
 
 from .models import Post, Comment, Like
 
 
+# Views for posts
 @api_view(['GET'])
 def posts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
