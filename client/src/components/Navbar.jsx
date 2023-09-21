1ca8fedca604a0b6a0c4f52d7fe331e2a61373c3
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-21 16:40:41 +0530

    add some data to homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { ImExit } from "react-icons/im";
+import logo from "../assets/logo.svg"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,40 +13,43 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 w-full bg-[#4d79ff] z-50 ${
+      className={`fixed top-0 p-4 w-full font-bold bg-[#ea3883] z-50 ${
         menuOpen ? "h-screen" : "h-auto"
       }`}
+      style={{
+        background: 'linear-gradient(to right, #abdafb, #1e2ae6)',
+      }}
     >
       <div className="max-w-6xl h-16 mx-auto flex justify-between items-center">
         <a href="/" className="text-white text-2xl font-semibold ml-4">
-          Logo
+          <img src={logo} alt="logo" className="w-16 h-16" />
         </a>
         <div
           className={`hidden md:flex space-x-4 ${
             menuOpen ? "hidden" : ""
           } transition-opacity duration-300 ease-in-out`}
         >
-          <a href="/" className="text-white text-lg hover:underline">
+          <a href="/" className="text-white text-xl hover:underline">
             Home
           </a>
-          <a href="/" className="text-white text-lg hover:underline">
+          <a href="/" className="text-white text-xl hover:underline">
             About
           </a>
-          <a href="/" className="text-white text-lg hover:underline">
+          <a href="/" className="text-white text-xl hover:underline">
             Cars
           </a>
-          <a href="/" className="text-white text-lg hover:underline">
+          <a href="/" className="text-white text-xl hover:underline">
             Rentals
           </a>
-          <a href="" className="text-white text-lg hover:underline">
+          <a href="" className="text-white text-xl hover:underline">
             Contact
           </a>
 
           <button
             onClick=""
-            className="flex flex-row justify-around text-white text-lg hover:underline"
+            className="flex flex-row justify-around text-white text-xl hover:underline"
           >
-            Sign out
+            Sign out<ImExit className="m-1 mx-2" />
           </button>
         </div>
         <div className="md:hidden mr-4">
@@ -86,7 +90,7 @@ const Navbar = () => {
         </div>
       </div>
       {menuOpen && (
-        <div className="flex flex-col justify-center my-16 items-center md:hidden bg-[#4d79ff] space-y-4 transition-opacity duration-1000 ease-in-out">
+        <div className="flex flex-col justify-center my-16 items-center md:hidden bg-[#ea3883] space-y-4 transition-opacity duration-1000 ease-in-out">
           <a
             href="/"
             className="block text-white p-2 text-3xl hover:bg-[#3c64e1]"
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Homepage = () => {
-  return (
-    <div>
-      
-      hellow 
-    </div>
-  )
-}
+  const [posts, setPosts] = useState([]); // Store posts in state
+  const [comments, setComments] = useState([]);
 
-export default Homepage
+  // Use useEffect to fetch data when the component mounts
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/v1/posts");
+        setPosts(response.data); // Set the fetched data in state
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    const getComments = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/api/v1/comments"
+        );
+        setComments(response.data); // Set the fetched data in state
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+
+    getPosts();
+    getComments();
+  }, []); // Empty dependency array means it runs once on component mount
+
+  return (
+    <div className="mt-24">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.name}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <h2>{comment.name}</h2>
+          <p>{comment.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Homepage;
